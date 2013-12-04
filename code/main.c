1736dbdf27e83dfaf436956949f2b43cd3f7c032
Danilo Bargen <gezuru@gmail.com>
2013-12-04 01:53:26 +0100

    Simple IR flash

--- code/main.c
+++ code/main.c
@@ -1,13 +1,42 @@
-#include <avr/io.h> 
-#include <util/delay.h> 
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define HPERIOD 0.01524
+#define RATIO 0.4
+#define NPULSES 16
+#define INSTANT 7.33
+#define DELAYED 5.36
+
+#define LEDON  0b00010111
+#define LEDOFF 0b00000001
+
+void flash_led(void) {
+    for(uint8_t i=0; i<NPULSES; i++) {
+        PORTB = LEDON;
+        _delay_ms(HPERIOD);
+        PORTB = LEDOFF;
+        _delay_ms(HPERIOD);
+    }
+}
 
 int main(void) {
-    DDRB = 0x01; //PB0 is set to output
+    // Set up I/O
+    DDRB = _BV(PB1) | _BV(PB2) | _BV(PB4); // PB0 is input, PB1/2/4 are output
+    PORTB = _BV(PB0); // Pull-up for input PB0
 
-    while(1) {  //Infinite loop
-        PORTB ^= 0x01; // Change PB0 bit, 0 to 1, 1 to 0
-        _delay_ms(200); // Delay
+    // Reduce power consumption
+    ADCSRA &= ~_BV(ADEN); // Disable ADC
+    ACSR &= ~_BV(ACD); // Disable the analog comparator
+
+    asm volatile ("nop");
+    asm volatile ("nop");
+
+    // Main loop
+    while(1) {
+        flash_led();
+        _delay_ms(INSTANT);
+        flash_led();
+
+        _delay_ms(1000);
     }
-
-    return 0;
 }
